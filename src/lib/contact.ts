e118b5b145ff22d6bd50ea10dda261e190151a4f
FASTPAGENWD <fastpage1024@gmail.com>
2026-07-13 15:44:34 +0200

    Telefoonnummer bijgewerkt naar +31 6 87754045

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -1,4 +1,4 @@
-export const PHONE_E164 = "+31685436044";
+export const PHONE_E164 = "+31687754045";
 export const PHONE_TEL = `tel:${PHONE_E164}`;
-export const PHONE_LABEL = "+31 6 85436044";
-export const WHATSAPP_URL = "https://wa.me/31685436044";
+export const PHONE_LABEL = "+31 6 87754045";
+export const WHATSAPP_URL = "https://wa.me/31687754045";
